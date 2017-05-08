c5a41ab38f0e5b266c7830dd99414f6447dcf278
Maximilian Kwok <max.kwok@gmail.com>
2017-05-08 11:12:57 -0700

    Commit - ClassName and Stringify both finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  	// your code here
+  	var results = [];
+  	var parent = document.body;
+	
+	var hasClass = function(element, cls) {
+	    return element.classList.contains(cls);
+	}
+	
+	var findInParent = function(bod){
+	  	if (hasClass(bod, className)){
+	  		results.push(bod);
+	  	}
+	  	if(bod.children.length > 0){
+		  	for (var i=0; i<bod.children.length; i++){
+		  		if (bod.children[i] !== undefined && !hasClass(bod.children[i], 'failures') && !hasClass(bod.children[i], 'duration') && !hasClass(bod.children[i], 'passes')
+		  			 && !hasClass(bod.children[i], 'progress')){
+			  		findInParent(bod.children[i]);
+		  		}else{
+		  			//console.log("==== undefined child found ====");
+	  			}
+	  		}
+	  	}
+	}
+
+  	findInParent(parent);
+  	return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,44 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'boolean' || typeof obj === 'number')
+  	return '' + obj + '';
+  if (typeof obj === undefined || typeof obj === 'function'){
+  	return '';
+  }
+  if (obj === null){
+  	return 'null';
+  }
+  if (typeof obj === 'string'){
+  	return '"' + obj + '"';
+  }
+  if (typeof obj === 'object'){
+  	if (Array.isArray(obj)){
+  		if (obj.length===0) { 
+  			return '[]'; 
+  		}else{
+	  		var returnStr = '';
+  			for (i=0; i<obj.length; i++){
+  				returnStr = returnStr + stringifyJSON(obj[i]) + ',';
+  			}
+  			returnStr = returnStr.slice(0,returnStr.length - 1);
+  			returnStr = '[' + returnStr + ']';
+  			return returnStr;
+  		}
+  	}else{
+  		if (_.isEmpty(obj)) { 
+  			return '{}'; 
+  		}else{
+  			var returnStr = '';
+  			for (var x in obj){
+  				if (obj[x] !== undefined && typeof obj[x] !== 'function'){
+	  				returnStr = returnStr + '"' + x + '":' + stringifyJSON(obj[x]) + ',';
+	  			}
+  			}
+  			returnStr = returnStr.slice(0,returnStr.length - 1);
+	  		returnStr = '{' + returnStr + '}';
+  			return returnStr;
+  		}
+  	}
+  }
 };
